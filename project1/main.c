e506178a58710eaf5ba881c12595cd1218198047
Kyuber <hkyeo98@gmail.com>
2021-09-08 22:56:53 +0900

    fix project1 and add Readme

--- project1/main.c
+++ project1/main.c
@@ -3,17 +3,25 @@
 float get_feps();
 double get_deps();
 
+void machar(int* ibeta, int* it, int* irnd, int* ngrd, int* machep, int* negep,
+	int* iexp, int* minexp, int* maxexp, float* eps, float* epsneg,
+	float* xmin, float* xmax);
+
+void machar_double(int* ibeta, int* it, int* irnd, int* ngrd, int* machep, int* negep,
+	int* iexp, int* minexp, int* maxexp, double* eps, double* epsneg,
+	double* xmin, double* xmax);
+
 int main() {
 	int ibeta, it, irnd, ngrd, machep, negep, iexp, minexp, maxexp;
 	float eps, epsneg, xmin, xmax;
 
-	machar(&ibeta, &it, &irnd, &irnd, &ngrd, &machep, &negep, &iexp, &minexp, &maxexp, &eps
+	machar(&ibeta, &it, &irnd, &ngrd, &machep, &negep, &iexp, &minexp, &maxexp, &eps
 	, &epsneg, &xmin, &xmax);
 
 	printf("eps %.23f\n", eps);
 
 	double deps, depsneg, dxmin, dxmax;
-	machar_double(&ibeta, &it, &irnd, &irnd, &ngrd, &machep, &negep, &iexp, &minexp, &maxexp, &deps
+	machar_double(&ibeta, &it, &irnd, &ngrd, &machep, &negep, &iexp, &minexp, &maxexp, &deps
 		, &depsneg, &dxmin, &dxmax);
 		
 	printf("deps %.52f\n", deps);
