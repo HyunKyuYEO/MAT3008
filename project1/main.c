a04986534571d369ab8dd15e20e9f9e1135b7ecc
Kyuber <hkyeo98@gmail.com>
2021-09-12 22:28:54 +0900

    project1 fixed

--- project1/main.c
+++ project1/main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<math.h>
-float get_feps();
-double get_deps();
+float float_eps();
+double double_eps();
 
 void machar(int* ibeta, int* it, int* irnd, int* ngrd, int* machep, int* negep,
 	int* iexp, int* minexp, int* maxexp, float* eps, float* epsneg,
@@ -18,35 +18,38 @@ int main() {
 	machar(&ibeta, &it, &irnd, &ngrd, &machep, &negep, &iexp, &minexp, &maxexp, &eps
 	, &epsneg, &xmin, &xmax);
 
-	printf("eps %.23f\n", eps);
+	printf("float eps by machar %.23f\n", eps);
 
 	double deps, depsneg, dxmin, dxmax;
 	machar_double(&ibeta, &it, &irnd, &ngrd, &machep, &negep, &iexp, &minexp, &maxexp, &deps
 		, &depsneg, &dxmin, &dxmax);
 		
-	printf("deps %.52f\n", deps);
+	printf("double eps by the machar_double %.52f\n", deps);
+	
+	printf("\n");
 
-	eps = get_feps();
-	deps = get_deps();
-	printf("feps = %.23f\n", eps);
-	printf("deps = %.52f\n", deps);
+	eps = float_eps();
+	deps = double_eps();
+	printf("float eps by my own function = %.23f\n", eps);
+	printf("double eps by my own function = %.52f\n", deps);
+
+	return 0;
 }
 
-float get_feps() {
-	
+float float_eps() {
 	float eps = 1;
 	do {
 		eps /= 2;
 	} while (eps + 1 > 1);
 
-	return eps;
+	return eps*2;
 }
 
-double get_deps() {
+double double_eps() {
 	double deps = 1;
 	do {
 		deps /= 2;
 	} while (deps + 1 > 1);
 	
-	return deps;
+	return deps*2;
 }
